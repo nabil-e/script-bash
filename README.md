a4e6a9a34537511dd252c19f88d7c9dfc9159224
nabil-e <nabilelassal@gmail.com>
2019-06-05 11:31:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# script-bash
+
+-> Cahier des charges : Objectifs du script <-
+
+
+
+
+
+* créer une plateforme de conteneurs similaire à des VM
+
+* pouvoir cleaner les conteneurs créés
+
+* pour avoir des infos : ip, nom, user
+
+* redémarrer les conteneurs si arrêtés
+
+* communication via ssh
+
+* système d'identification des conteneurs produit par le script
+
+* à découvrir : affichage, conditions, variables, itérations, arguments, docker, boucles
---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+########################################################
+#
+# Description : lancement d'une plateforme de conteneurs
+#
+# Auteur : nabil-e
+#
+# Date : 05/06/2019
+#
+########################################################
+
+echo "
+
+Options :
+
+	--create : si aucun chiffre 2 conteneurs sinon x conteneurs seront créés
+
+	--drop : pour supprimer les conteneurs créés via ce script par ${USER}
+
+	--start : pour redémarrer les conteneurs
+
+	--infos : pour récapituler les infos des conteneurs (user, ip, nom...)
+
+	--ansible : pour créer une base de dev pour ansible
+
+"
